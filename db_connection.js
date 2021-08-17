fa35ca2512def7cb43a2c83f0f60b4525d0fa96e
jackylearn <jacky3627jp@gmail.com>
2021-08-17 14:17:33 +0800

    remove redundant async await

--- db_connection.js
+++ db_connection.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 const mongoose = require('mongoose')
 
-async function connectDatabase() {
+function connectDatabase() {
     const URI = process.env.MONGODB_URI
-    await mongoose.connect(URI, { useNewUrlParser: true, useFindAndModify: false });
+    mongoose.connect(URI, { useNewUrlParser: true, useFindAndModify: false });
     const db = mongoose.connection
     db.on('error', console.error.bind(console, 'connection error:'))
     db.once('open', () => console.log("db is connected."))
