aa036580a14602827a4edd7e77e228914f67a16c
jackylearn <jacky3627jp@gmail.com>
2021-08-17 13:58:34 +0800

    db connection message

--- db_connection.js
+++ db_connection.js
@@ -1,11 +1,12 @@
 require('dotenv').config();
 const mongoose = require('mongoose')
-mongoose.set();
 
 async function connectDatabase() {
     const URI = process.env.MONGODB_URI
     await mongoose.connect(URI, { useNewUrlParser: true, useFindAndModify: false });
-
+    const db = mongoose.connection
+    db.on('error', console.error.bind(console, 'connection error:'))
+    db.once('open', () => console.log("db is connected."))
     return Promise.resolve(mongoose.connection.client)
 }
 
