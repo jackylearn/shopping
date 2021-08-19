43e83f967499526a4f092497301899f32cf425d6
jackylearn <jacky3627jp@gmail.com>
2021-08-19 10:55:49 +0800

    modal popup animation

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,43 @@
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+const { useEffect, useState } = require('react')
 
 function App() {
+  const [registerModalShown, setRegister] = useState(false)
+  const [loginModalShown, setLogin] = useState(false)
 
 
   const handleFormSwitch = (event) => {
-    console.log('hi')
+    setRegister(prev => !prev)
+    setLogin(prev => !prev)
   }
 
+  const loginButtonPressed = () => {
+    setLogin(true)
+    setRegister(false)
+  }
+
+  const registerButtonPressed = () => {
+    setRegister(true)
+    setLogin(false)
+  }
+
+  const closeModal = () => {
+    setRegister(false)
+    setLogin(false)
+  }
 
   return (
     <div className="app">
+      <Navbar loginButton={loginButtonPressed} registerButton={registerButtonPressed} />
       <Modal
         name="Register"
         route="/register"
         message="Already has an account?"
         link="login"
         handleFormSwitch={handleFormSwitch}
-
+        status={registerModalShown}
       />
       <Modal
         name="Login"
@@ -24,19 +45,22 @@ function App() {
         message="Do not have an account?"
         link="register"
         handleFormSwitch={handleFormSwitch}
-        status="hidden"
+        status={loginModalShown}
       />
       <div
         id="overlay"
-        className="hidden"
+        className={loginModalShown || registerModalShown ? "active" : ""}
+        onClick={closeModal}
       ></div>
     </div>
   );
 }
 
 function Modal(props) {
+  const modalClass = props.status ? "modal active" : "modal";
+
   return (
-    <div id={props.name.toLowerCase()} className={`modal ${props.status}`} >
+    <div id={props.name.toLowerCase()} className={modalClass} >
       <h3 className="modal-title">{props.name}</h3>
       <form action={props.route} method='post'>
         <div className="form-content">
@@ -57,5 +81,21 @@ function Modal(props) {
 
   )
 }
+
+function Navbar(props) {
+  return (
+    <nav id="navbar">
+      <div id="logo">BookRental</div>
+      <div>
+
+        <span onClick={props.loginButton}>Login</span>
+        <span onClick={props.registerButton}>Register</span>
+        <span><FontAwesomeIcon icon={["fas", "coffee"]} />Shopping Cart</span>
+      </div>
+    </nav>
+  )
+}
+
+
 
 export default App;
