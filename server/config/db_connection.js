f525eeb5654a3b6bc74cd36e72a36bd63be3fca1
jackylearn <jacky3627jp@gmail.com>
2021-08-19 11:19:59 +0800

    add process exit when db connection fail

---
+++ server/config/db_connection.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const mongoose = require('mongoose')
+
+const { MongoClient } = require('mongodb')
+
+async function connectDatabase() {
+    const URI = process.env.MONGODB_URI
+    try {
+        await mongoose.connect(URI, { useNewUrlParser: true, useFindAndModify: false });
+        console.log("db is connected.")
+    } catch (err) {
+        console.log("Failed to connect to Mongodb.")
+        process.exit(1)
+    }
+
+    return Promise.resolve()
+}
+
+async function disconnect() {
+    await mongoose.connection.close()
+}
+
+module.exports.connect = connectDatabase;
+module.exports.disconnect = disconnect;
