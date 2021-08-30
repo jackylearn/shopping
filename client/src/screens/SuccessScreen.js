28aa47ba943486e72410871fc1550f59c82835d8
jackylearn <jacky3627jp@gmail.com>
2021-08-30 15:52:30 +0800

    add timer and redirect

--- client/src/screens/SuccessScreen.js
+++ client/src/screens/SuccessScreen.js
@@ -1,28 +1,41 @@
 import './SuccessScreen.css'
 import { Link } from 'react-router-dom'
 
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { resetCart } from '../redux/actions/cartActions.js'
 import axios from 'axios'
 
-export default function SuccessScreen({ history }) {
 
+
+export default function SuccessScreen({ history }) {
+    const [timeLeft, setTimer] = useState(5)
     const dispatch = useDispatch();
     const userId = useSelector(state => state.auth.data?.id)
     const purchasedBooks = Object.keys(useSelector(state => state.cart))
-    console.log(purchasedBooks)
 
     useEffect(() => {
         if (userId)
             axios.put(`/api/user/${userId}/purchasedBooks`, { purchasedBooks })
         dispatch(resetCart())
-    }, [history]);
+    }, []);
+
+    useEffect(() => {
+        setTimeout(() => { window.location.replace('/') }, 5000)
+    })
+
+    useEffect(() => {
+        const timer = setTimeout(() => setTimer(prev => prev - 1), 1000)
+        return () => {
+            clearTimeout(timer)
+        }
+    })
 
 
     return (
         <div className='success-screen'>
             <h2>payment success</h2>
+            <p>{`${timeLeft} secs redirects to shop`}</p>
             <Link id='back-to-shop' to={'/'} > Back to shop </Link>
         </div>
     )
